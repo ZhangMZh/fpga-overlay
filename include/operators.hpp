299fb73c219d9dad6dd0aade3f98b1b87e080a28
MZZhang <zmz21@mails.tsinghua.edu.cn>
2023-03-05 23:21:34 -0800

    Enable dot product emulation

---
+++ include/operators.hpp
@@ -0,0 +1,7 @@
+#ifndef __OPERATORS_HPP__
+#define __OPERATORS_HPP__
+#include <sycl/sycl.hpp>
+
+SYCL_EXTERNAL extern "C" float dot_prod_16(sycl::float16 x, sycl::float16 y);
+
+#endif //__OPERATORS_HPP__
