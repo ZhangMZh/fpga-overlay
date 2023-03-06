f2a5978d3479eb67cd07e2b85c25cc39eab165cd
MZZhang <zmz21@mails.tsinghua.edu.cn>
2023-03-05 23:27:18 -0800

    Clean repo

--- library_matrix_mult/device/inc/dot_prod.hcl
+++
@@ -1,3 +0,0 @@
-// Header file for use from OpenCL
-
-void dot_prod(__global float* A, __global float* B, uint n);
--- library_matrix_mult/device/lib/dot_prod.h
+++
@@ -1,8 +0,0 @@
-// Only use OCL address spaces if in OCL flow. Otherwise use empty define.
-#ifdef TARGET_OCL
-#include "HLS/ocl_types.h"
-#else
-#define OCL_ADDRSP_GLOBAL
-#endif
-
-extern "C" float dot_prod(OCL_ADDRSP_GLOBAL float* A, OCL_ADDRSP_GLOBAL float* B, unsigned n);
